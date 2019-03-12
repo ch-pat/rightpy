e4930bdf59288d49547361b10a0274af8bb2dfb2
Patryk <patryk.choma@gmail.com>
2019-03-12 17:50:46 +0100

    First upload

---
+++ algebra.py
@@ -0,0 +1,7 @@
+'''
+This python file contains all functions and classes I come up with as I read Linear Algebra 
+Done Right complete with in-depth annotations for the meaning of each function as I understand it.
+I hope that this helps deepen my understanding of linear algebra and that it can be used by others
+to help in their understanding as well.
+'''
+
